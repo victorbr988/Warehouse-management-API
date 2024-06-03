3ac439e409fc447cde7f78b8d15810d6afc7a943
Victor <victor.lins@treetech.com.br>
2024-06-03 18:32:48 -0300

    feat: add more product endpoints

--- src/controller/product/product.controller.ts
+++ src/controller/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpStatus, Post, Res } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Delete, HttpStatus, Param, Post, Put, Res } from "@nestjs/common";
 import { ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Response } from "express";
 import { ProductDto } from "src/repository/dtos/product.dto";
@@ -21,6 +21,43 @@ export class ProductController {
     try {
       const { name, quantity } = productDto;
       await this.productService.createProduct({ name, quantity });
+      return res.status(HttpStatus.CREATED).json({ message: "Product created successfully" });
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: error.message });
+    }
+  }
+
+  @Put("update")
+  @ApiResponse({ status: 201, description: 'Product created successfully' })
+  @ApiResponse({ status: 404, description: 'Not Found' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  public async updateProduct(@Param() id: string, @Body() productDto: ProductDto, @Res() res: Response) {
+    try {
+      if (!id) {
+        throw new BadRequestException("Id is required");
+      }
+
+      const { name, quantity } = productDto;
+      await this.productService.updateProduct(id, { name, quantity });
+      return res.status(HttpStatus.CREATED).json({ message: "Product created successfully" });
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: error.message });
+    }
+  }
+  
+  @Delete("delete")
+  @ApiResponse({ status: 201, description: 'Product created successfully' })
+  @ApiResponse({ status: 404, description: 'Not Found' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  public async deleteProduct(@Param() id: string, @Res() res: Response) {
+    try {
+      if (!id) {
+        throw new BadRequestException("Id is required");
+      }
+
+      await this.productService.deleteProduct(id);
       return res.status(HttpStatus.CREATED).json({ message: "Product created successfully" });
     } catch (error) {
       return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: error.message });
--- src/controller/product/product.service.ts
+++ src/controller/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { createId } from "@paralleldrive/cuid2";
 import { ProductDto } from "src/repository/dtos/product.dto";
@@ -27,5 +27,46 @@ export class ProductService {
       .execute();
     
     return "Product created";
+  }
+
+  public async updateProduct(id: string, productDto: ProductDto) {
+    const { name, quantity } = productDto;
+    const product = await this.productRepository.findOneBy({ id });
+
+    if (!product) {
+      throw new NotFoundException();
+    }
+
+    const productDatabase = {
+      ...product,
+      name,
+      quantity: quantity || product.quantity
+    }
+
+    await this.productRepository
+      .createQueryBuilder()
+      .update(Product)
+      .set(productDatabase)
+      .where("id = :id", { id })
+      .execute();
+
+    return "Product updated";
+  }
+
+  public async deleteProduct(id: string) {
+    const product = await this.productRepository.findOneBy({ id });
+
+    if (!product) {
+      throw new Error("Product not found");
+    }
+
+    await this.productRepository
+      .createQueryBuilder()
+      .delete()
+      .from(Product)
+      .where("id = :id", { id })
+      .execute();
+
+    return "Product deleted";
   }
 }
